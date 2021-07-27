8561767c6024451996d492b96c3d78f187387897
Remarc Balisi <remarc.balisi@gmail.com>
2021-07-27 16:18:25 +0800

    Update InitializeDataDisk.ps1

--- InitializeDataDisk.ps1
+++ InitializeDataDisk.ps1
@@ -1,5 +1,5 @@
 Get-Disk | 
-Where partitionstyle -eq `raw' | 
+Where partitionstyle -eq 'raw' | 
 Initialize-Disk -PartitionStyle MBR -PassThru |
 New-Partition -DriveLetter F -UseMaximumSize | 
 Format-Volume -FileSystem NTFS -NewFileSystemLabel "disk2" -Confirm:$false
