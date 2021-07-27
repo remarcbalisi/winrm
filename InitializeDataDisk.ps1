1d6c07f9b66e03467dfd0abd1bdd07ef9cbe2a72
Remarc Balisi <remarc.balisi@gmail.com>
2021-07-27 16:35:45 +0800

    Update InitializeDataDisk.ps1

--- InitializeDataDisk.ps1
+++ InitializeDataDisk.ps1
@@ -3,4 +3,4 @@ Where partitionstyle -eq 'raw' |
 Initialize-Disk -PartitionStyle MBR -PassThru |
 New-Partition -DriveLetter F -UseMaximumSize | 
 Format-Volume -FileSystem NTFS -NewFileSystemLabel "disk2" -Confirm:$false | 
-New-Item -Path "D:\" -Name "VMs" -ItemType "directory"
+New-Item -Path "F:\" -Name "VMs" -ItemType "directory"
